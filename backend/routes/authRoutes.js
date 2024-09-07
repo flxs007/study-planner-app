deece548a5352dbb91f14d03974e48bbcb515680
Felix <165252170+flxs007@users.noreply.github.com>
2024-09-08 00:39:32 +0900

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');  // Import authentication middleware
+const rateLimit = require('express-rate-limit');
+
+const loginLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5, // 5 reqs oer ip
+    message: "Too many login attempts, please try again later."
+  });
+
+router.post('/signup', authMiddleware, registerUser);  // Ensure user is authenticated and has appropriate role
+router.post('/login', loginLimiter, loginUser);
 
 
-router.post('/signup', registerUser); 
-router.post('/login', loginUser); 
 
-module.exports = router; 
+module.exports = router;
