059914e226c5ae02c062f8f42d0791d6e19541df
Felix <165252170+flxs007@users.noreply.github.com>
2024-09-08 00:41:44 +0900

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,17 @@
+// connectDB.js
 const mongoose = require('mongoose');
-require('dotenv').config(); // load env
+require('dotenv').config(); // Load environment variables
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("MongoDB connected");
     } catch (error) {
         console.error("MongoDB connection failed", error.message);
-        process.exit(1); // exit
+        process.exit(1);
     }
 };
 
