64f0849a9bf9bcda903e29e13e3dc1615b5e9bb5
Felix <165252170+flxs007@users.noreply.github.com>
2024-09-08 00:39:46 +0900

    Update Course.js

--- backend/models/Course.js
+++ backend/models/Course.js
@@ -1,22 +1,31 @@
+// models/Course.js
 const mongoose = require('mongoose');
 
 const courseSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    instructor: {
-        type: String,
-        required: true,
-    },
-    userId: {
-        type: String,  
-        required: false,  
-    },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  instructor: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: false,
+  },
+  deadline: {
+    type: Date,
+    required: false,
+  },
+  deleted: {  // Add a 'deleted' field to manage soft deletes
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model('Course', courseSchema);
