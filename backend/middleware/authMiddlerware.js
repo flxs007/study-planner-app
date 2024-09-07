907008786c5f80928774b3b1b13f9a49eafb5aab
Felix <165252170+flxs007@users.noreply.github.com>
2024-09-08 00:40:35 +0900

    Update authMiddlerware.js

--- backend/middleware/authMiddlerware.js
+++ backend/middleware/authMiddlerware.js
@@ -1,16 +1,22 @@
+// middleware/authMiddleware.js
 const jwt = require('jsonwebtoken');
-const User = require('../models/User'); 
+const User = require('../models/User');
 
 const authMiddleware = async (req, res, next) => {
-  const token = req.header('Authorization')?.replace('Bearer ', ''); // Get token from headers
+  const token = req.header('Authorization')?.replace('Bearer ', '');
 
   if (!token) {
     return res.status(401).json({ message: 'Access Denied' });
   }
 
   try {
-    const verified = jwt.verify(token, process.env.JWT_SECRET); // verify the token
-    req.user = await User.findById(verified.id); // Add user info to the request
+    const verified = jwt.verify(token, process.env.JWT_SECRET);  // verify the token
+    req.user = await User.findById(verified.id);  // Add user info to the request
+
+    if (!req.user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
     next();
   } catch (err) {
     return res.status(400).json({ message: 'Invalid Token' });
